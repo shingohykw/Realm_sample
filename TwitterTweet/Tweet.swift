6a2fabe3d835da372b88d00f88f63f575156f243
Shingo Hayakawa <shingohykw@gmail.com>
2021-09-18 14:59:38 +0900

    swift3->5 リファクタリング： Tweet, Userファイルのモデル定義の各項目に@objcを追加　->実行時エラー解消

--- TwitterTweet/Tweet.swift
+++ TwitterTweet/Tweet.swift
@@ -15,10 +15,10 @@ import RealmSwift
  */
 class Tweet: Object {
     
-    dynamic var id = 0 // 一意のID
-    dynamic var created_at = "" // 作成された日付
-    dynamic var text = "" // 本文
-    dynamic var user: User? // ツイートを投稿したユーザ。ユーザモデルへの1対1の関連。
+    @objc dynamic var id = 0 // 一意のID
+    @objc dynamic var created_at = "" // 作成された日付
+    @objc dynamic var text = "" // 本文
+    @objc dynamic var user: User? // ツイートを投稿したユーザ。ユーザモデルへの1対1の関連。
     
     /**
      読みやすいフォーマットに変更した日付文字列を返す。
--- TwitterTweet/User.swift
+++ TwitterTweet/User.swift
@@ -15,10 +15,10 @@ import RealmSwift
  */
 class User: Object {
     
-    dynamic var id = 0 // 一意の数値ID(変更不可でTwitterアカウントを作成時に自動で与えられる)
-    dynamic var name = "" // 名前
-    dynamic var screen_name = "" // スクリーンネーム（変更可能な一意の文字列ID）
-    dynamic var profile_image_url_https = "" // プロフィール画像URL
+    @objc dynamic var id = 0 // 一意の数値ID(変更不可でTwitterアカウントを作成時に自動で与えられる)
+    @objc dynamic var name = "" // 名前
+    @objc dynamic var screen_name = "" // スクリーンネーム（変更可能な一意の文字列ID）
+    @objc dynamic var profile_image_url_https = "" // プロフィール画像URL
     
     /**
      プライマリキーとするプロパティ名を返す。
