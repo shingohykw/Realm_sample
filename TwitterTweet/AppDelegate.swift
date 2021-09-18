8f8d62cfad9bf7c4b8b957827b110252cd197054
Shingo Hayakawa <shingohykw@gmail.com>
2021-09-18 11:10:42 +0900

    swift 3.0 -> 5へリファクタリング：Replace 'UIApplicationLaunchOptionsKey' with 'UIApplication.LaunchOptionsKey'

--- TwitterTweet/AppDelegate.swift
+++ TwitterTweet/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print("19.2 ツイートを表示する(p138)")
         return true
     }
